a92d18d59041767cea5d4d1653a3793d485d30bc
virgiawankusuma <virgiawan150@gmail.com>
2021-11-14 22:31:14 +0700

    fix: configure webpack based on environtment

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,23 @@
-const path = require('path');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-    entry: '.src/index.js',
-    output: {
-        path: path.resolve(_dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    mode: 'production'
-}
+
+module.exports = merge(common, {
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+})

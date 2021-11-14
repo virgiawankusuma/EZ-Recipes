b794ead67897261eba30a803ae667371eac97583
virgiawankusuma <virgiawan150@gmail.com>
2021-11-14 22:05:23 +0700

    feat: add HTMLWebpack Plugin and Configure it

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -27,5 +28,11 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            filename: "index.html"
+        })
+    ]
 }

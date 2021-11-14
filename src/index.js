2640d7b3ef9f23608f63d430cf808623e97cdddb
virgiawankusuma <virgiawan150@gmail.com>
2021-11-15 00:03:14 +0700

    feat: add bootstrap icon, navbar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style/style.css";
-
 import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "ajax";
--- webpack.common.js
+++ webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./index.html",
+            template: "./src/index.html",
             filename: "index.html"
         })
     ]

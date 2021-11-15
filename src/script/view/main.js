f6821f3c9b79536205204d1061a4f53f232f2dea
virgiawankusuma <virgiawan150@gmail.com>
2021-11-15 15:36:16 +0700

    fix: add custom shadow navbar with eventlistener, fix responsive card

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,43 +1,54 @@
 import Source from '../data/source.js';
 
-const main = () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
-    const clubListElement = document.querySelector("#clubList");
+const navbar = document.querySelector(".navbar");
 
-    const onButtonSearchClicked = async () => {
-        try {
-            const result = await Source.searchClub(searchElement.value);
-            renderResult(result);
-        } catch (message) {
-            fallbackResult(message)
-        }
-    };
+window.addEventListener("scroll", (event) => {
+    const scrollY = window.scrollY;
+    if (scrollY < 20) {
+        navbar.classList.remove("shadow-sm");
+    } else {
+        navbar.classList.add("shadow-sm");
+    }
+});
 
-    const renderResult = results => {
-        clubListElement.innerHTML = "";
-        results.forEach(club => {
-            const { name, fanArt, description } = club;
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
+// const main = () => {
+//     const searchElement = document.querySelector("#searchElement");
+//     const buttonSearchElement = document.querySelector("#searchButtonElement");
+//     const clubListElement = document.querySelector("#clubList");
 
-            clubElement.innerHTML = `
-               <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-               <div class="club-info">
-                   <h2>${name}</h2>
-                   <p>${description}</p>
-               </div>`;
+//     const onButtonSearchClicked = async () => {
+//         try {
+//             const result = await Source.searchClub(searchElement.value);
+//             renderResult(result);
+//         } catch (message) {
+//             fallbackResult(message)
+//         }
+//     };
 
-            clubListElement.appendChild(clubElement);
-        })
-    };
+//     const renderResult = results => {
+//         clubListElement.innerHTML = "";
+//         results.forEach(club => {
+//             const { name, fanArt, description } = club;
+//             const clubElement = document.createElement("div");
+//             clubElement.setAttribute("class", "club");
 
-    const fallbackResult = message => {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
-    };
+//             clubElement.innerHTML = `
+//                <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+//                <div class="club-info">
+//                    <h2>${name}</h2>
+//                    <p>${description}</p>
+//                </div>`;
 
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
-};
+//             clubListElement.appendChild(clubElement);
+//         })
+//     };
+
+//     const fallbackResult = message => {
+//         clubListElement.innerHTML = "";
+//         clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+//     };
+
+//     buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+// };
 
 export default main;

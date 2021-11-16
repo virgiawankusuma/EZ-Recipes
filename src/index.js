3d38a1bd3ad103b2fb9ae569b15a4ece6c6fea40
virgiawankusuma <virgiawan150@gmail.com>
2021-11-16 15:09:37 +0700

    fix: add @propperjs/core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import "regenerator-runtime";
 import "./style.css";
-import "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import view from './script/view.js';
 
